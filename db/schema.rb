a8f3c5cfd7d8316c4668cecb7183d14e46bc55f2
Oskar Pietrzyk <oskpie97@gmail.com>
2021-05-16 22:47:34 +0200

    Create computers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_152344) do
+ActiveRecord::Schema.define(version: 2021_05_16_194926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "computers", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.uuid "uuid", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
